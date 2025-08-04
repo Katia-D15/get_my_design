bb6a55a84b985386428e30684584f8e157a7a762
Katia-D15 <0j0zirbo@students.codeinstitute.net>
2025-08-04 19:32:45 +0100

    feat: add checkout success view/page and loading overlay

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('<str:order_number>/', views.checkout, name='checkout'),
+    path(
+        'checkout_success/<str:order_number>/',
+        views.checkout_success,
+        name='checkout_success'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
+from django.contrib import messages
 import stripe
 from orders.models import Order
 
 
 def checkout(request, order_number):
+    """
+    Handle the checkout process for an existing order
+    """
     order = get_object_or_404(Order, order_number=order_number)
+
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    if request.method == 'POST':
+        if order.status == 'pending':
+            order.status = 'confirmed'
+            order.save()
+        return redirect('checkout_success', order_number=order.order_number)
 
     stripe_total = round(order.price * 100)
     stripe.api_key = stripe_secret_key
@@ -23,3 +34,22 @@ def checkout(request, order_number):
         'client_secret': intent.client_secret,
               }
     return render(request, 'checkout/checkout.html', context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.add_message(
+        request,
+        messages.SUCCESS,
+        f'Order successfully Confirmed!'
+        f'A confirmation email will be sent to {order.user.email}')
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+    return render(request, template, context)
